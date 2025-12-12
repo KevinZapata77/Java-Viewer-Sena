1279763cca60b112a6f4a562126463ff0caf1574
Luis Mendez <slpmendez60@gmail.com>
2025-12-12 00:06:00 -0500

    agregar lógica de reproducción

--- src/main/java/viewer/Contenido.java
+++ src/main/java/viewer/Contenido.java
@@ -1,23 +1,37 @@
-package viewer.models;
+package viewer;
 
 public class Contenido {
     protected String titulo;
-    protected int duracion;
-    protected String categoria;
+    protected String genero;
+    protected int duracion; // Minutos
+    protected boolean visto = false;
 
-    public Contenido(String titulo, int duracion, String categoria) {
+    public Contenido(String titulo, String genero, int duracion) {
         this.titulo = titulo;
+        this.genero = genero;
         this.duracion = duracion;
-        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public boolean isVisto() {
+        return visto;
+    }
+
+    public void marcarVisto() {
+        this.visto = true;
     }
 
     public void reproducir() {
-        System.out.println("Reproduciendo: " + titulo);
+        System.out.println("Reproduciendo: " + titulo + "...");
     }
 
     public void mostrarInfo() {
         System.out.println("Título: " + titulo);
+        System.out.println("Género: " + genero);
         System.out.println("Duración: " + duracion + " min");
-        System.out.println("Categoría: " + categoria);
+        System.out.println("Visto: " + (visto ? "Sí" : "No"));
     }
 }
--- src/main/java/viewer/Pelicula.java
+++
@@ -1,8 +0,0 @@
-package viewer.models;
-
-public class Pelicula extends Contenido {
-
-    public Pelicula(String titulo, int duracion, String categoria) {
-        super(titulo, duracion, categoria);
-    }
-}
